8fcc042a4f515dc842d59e2b9b3886751c99e431
Tenzit <tenzitmail@gmail.com>
2022-09-15 20:07:59 -0500

    Add output to custom structs
    Do the overload on operator<<

--- Hunting_Data_Collection/DataClass.h
+++ Hunting_Data_Collection/DataClass.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "string"
+#include <iostream>
+#include <ostream>
 
 struct PieceTimes {
 	int il;
@@ -10,9 +12,28 @@ struct PieceTimes {
 	int timesRestarted;
 };
 
+std::ostream &operator<< (std::ostream& os, const PieceTimes& rhs) {
+	os << rhs.il << ","
+		<< rhs.igt << ","
+		<< rhs.v1 << ","
+		<< rhs.v2_5 << ","
+		<< rhs.timesRestarted;
+	return os;
+};
+
 struct Data {
 	std::string stageName;
 	uint32_t pieces[3];
 	PieceTimes collected[3];
 	PieceTimes finished;
+};
+
+std::ostream &operator<< (std::ostream& os, const Data& rhs) {
+	os << rhs.stageName << ",";
+	for (int i = 0; i < 3; i++) {
+		os << rhs.pieces[i] << ","
+			<< rhs.collected[i] << ",";
+	}
+	os << rhs.finished;
+	return os;
 };
